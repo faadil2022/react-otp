f486b93112d611c6b7c8a8499f0c6cbbdd7d3adb
faadil2022 <87930931+faadil2022@users.noreply.github.com>
2021-08-18 12:51:50 +0530

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,8 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 var firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+
+
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
