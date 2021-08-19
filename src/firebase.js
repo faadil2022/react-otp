743899d2a22881fc94e19e0564e8d4922181a3e4
faadil2022 <87930931+faadil2022@users.noreply.github.com>
2021-08-19 20:02:02 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
-var firebaseConfig = {
+ firebaseConfig = {
 
+    apiKey: "AIzaSyBYjEHuER5gKpWFahx7evhQj9vHdVILJNY",
+
+    authDomain: "verify-otp-a4646.firebaseapp.com",
+
+    projectId: "verify-otp-a4646",
+
+    storageBucket: "verify-otp-a4646.appspot.com",
+
+    messagingSenderId: "322497483335",
+
+    appId: "1:322497483335:web:ede3c9b4bbd33ac1789762"
 
   };
   // Initialize Firebase
